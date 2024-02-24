ff3869fac1881e72813d22dffdba31da57292999
hidekitakarada <115335000+hidekitakarada@users.noreply.github.com>
2024-02-24 23:07:05 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,5 @@ if uploaded_file is not None:
         na_values='-',
         header=None)
 
-st.markdown('### アクセスログ（先頭5件）')
-st.write(df.head(5))
+    st.markdown('### アクセスログ（先頭5件）')
+    st.write(df.head(5))
